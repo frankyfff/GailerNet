7b75b86f858019113ec29d0c52b5aed1e6b32853
Moe <schaunach@gmail.com>
2018-10-17 13:21:11 +0200

    tmp

---
+++ GailerJava/Uebungen/Kapitel_29/NullDemo1.java
@@ -0,0 +1,25 @@
+package Kapitel_29;
+
+public class NullDemo1 {
+
+	public static void main(String[] args) {
+		
+		String a = "Zufaellige Notizen";
+		String b = null;
+		String c = "";
+		
+		if (a != null) {
+			System.out.println(a);
+		}
+		
+		if (b != null) {
+			System.out.println(b);
+		}
+		
+		if (c != null) {
+			System.out.println(c);
+		}
+
+	}
+
+}
---
+++ GailerJava/Uebungen/Kapitel_29/ToLowerCaseDemo.java
@@ -0,0 +1,21 @@
+package Kapitel_29;
+
+public class ToLowerCaseDemo {
+
+	public static void main(String[] args) {
+		
+		String zeile = "Der Himmel war wie ein Wassertropen";
+		
+		String a = zeile.toLowerCase();
+		
+	//	String b = toLowerCase( zeile );
+		
+	//	String c = toLowerCase( "IM SCHATTEN von THOR" );
+		
+		String d = "Klar, Ruhig, Schon".toLowerCase();
+		
+		System.out.println( "Finster, verlassen...".toLowerCase() );
+
+	}
+
+}
